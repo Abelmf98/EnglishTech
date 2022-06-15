0508e88567fa5f3ccf7373f34ba09043edcaf5d7
Abelmf98 <91120898+Abelmf98@users.noreply.github.com>
2022-06-15 20:18:17 +0200

    script de los nombres y contraseñas

---
+++ sql/insertUsuariosEnc.sql
@@ -0,0 +1,7 @@
+INSERT INTO usuario (nombre, contrasenia, tipo)
+    VALUES ('isabel', '1234', 'a'),
+            ('ernesto' , '1234', 'u'),
+            ('miguel', '1234', 'a');
+
+-- password encriptada:
+-- $2y$10$GSQ9NbOAe0XCdUk/xi9m3OapGxh/p/M3slImlcBJQ8PbOnG9ie43G
